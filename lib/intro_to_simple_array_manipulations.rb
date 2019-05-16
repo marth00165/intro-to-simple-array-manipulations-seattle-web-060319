53bd91030ccab73847bc0bd4073b11fb821aa860
Rohit Pratti <rohit.pratti@gmail.com>
2019-05-16 23:57:46 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,48 @@
+def using_push(chicken, noodles)
+  chicken.push(noodles)
+end
+
+def using_unshift(goku, vegeta)
+  goku.unshift(vegeta)
+end
+
+def using_pop(madara)
+  madara.pop 
+end
+
+def pop_with_args(greed)
+  greed.pop(2)
+end
+
+def using_shift(tobi)
+  tobi.shift 
+end
+
+def shift_with_args(fullMetal)
+  fullMetal.shift(2)
+end
+
+def using_concat(one, two)
+  one.concat(two)
+end
+
+def using_insert(sasuke, chidori)
+  sasuke.insert( 4 ,chidori)
+end
+
+def using_uniq(royMustang)
+  royMustang.uniq 
+end
+
+def using_flatten(donQ)
+  donQ.flatten 
+end
+
+def using_delete(nas, jayZ)
+  nas.delete(jayZ)
+end
+
+def using_delete_at(theGame, gUnit)
+  theGame.delete_at(gUnit)
+end
+
